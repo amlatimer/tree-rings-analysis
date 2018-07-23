1ea940c0e82fdd90bab4fbaf3b7e4ac0cda1e071
amlatimer <amlatimer@ucdavis.edu>
2018-07-23 16:15:15 -0700

    compare graphically the growth trends for trees where basal area is measured from core out, vs from bark in.

--- scripts/prelim-analysis.R
+++ scripts/prelim-analysis.R
@@ -4,6 +4,7 @@
 library(ggplot2)
 library(sp)
 library(lme4)
+library(maps)
 
 years <- read.csv("../data/years.csv")
 trees <- read.csv("../data/trees.csv")
@@ -18,11 +19,29 @@ min(years$year); max(years$year) # how many years (55)
 length(unique(trees$plot.id)) # how many plots? (63)
 table(trees$species) # how many trees of each species?
 
+# Add cluster ID to trees data set
+trees <- merge(trees, plots[, c("plot.id", "cluster")], sort=FALSE, by = "plot.id", all=FALSE)
+# Add cluster and plot ID and species to years data set
+years <- merge(years, trees[, c("tree.id", "plot.id", "cluster", "species")], sort=FALSE, by = "tree.id", all=FALSE)
+
+# how many trees have BAI data for each year?
+with(years, table(bai.pres=!is.na(bai.comb), year)) # most years have values for most trees
+with(years, table(bai.pres=!is.na(bai), year)) # about 2/3 of trees have "pith-based" BA for any given year. 
+table(years$tree.id[!is.na(years$ba)])
+table(years$cluster[!is.na(years$ba)])
+table(years$cluster[!is.na(years$ba.comb)])
+
 # Where are the plots?
 treelocs <- SpatialPoints(coords=trees[,c("x", "y")], proj4string = CRS("+proj=albers"))
-map()
 plot(treelocs, pch=16, col=trees$species)
 
-# Add cluster ID to trees data set 
-trees <- merge(trees, plots[, c("plot.id", "cluster")], sort=FALSE, by = "plot.id", all=FALSE)
+# visulize some individual tree growth trends for trees with basal area measured from inside out, and from outside in. 
+years_psme <- years[years$species=="PSME",]
+unique(plots$plot.id)
+p <- ggplot(years[years$plot.id=="ULC1" & years$species=="PSME",], aes(year, ba.comb)) + geom_line(aes(color = !is.na(ba))) + facet_wrap(~tree.id)
+p
+# No obvious difference between trees' basal area that's measured from core out (ba, bai) and basal area that's measured from outside in (ba.ext, bai.ext). Therefore, proceed with using the combined data for now (ba.comb, bai.comb). 
+
+
+
 
