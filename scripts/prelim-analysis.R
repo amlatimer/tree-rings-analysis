c7aee2fb0e80467b1b574edd46102300a8c5dab1
amlatimer <amlatimer@ucdavis.edu>
2018-07-24 09:38:14 -0700

    Check for growth rate differences among species

--- scripts/prelim-analysis.R
+++ scripts/prelim-analysis.R
@@ -19,10 +19,18 @@ min(years$year); max(years$year) # how many years (55)
 length(unique(trees$plot.id)) # how many plots? (63)
 table(trees$species) # how many trees of each species?
 
+# fix problem with PSME id
+levels(trees$species)
+trees$species[trees$species==""] <- NA
+trees$species[trees$species=="PSME "] <- "PSME"
+trees$species <- droplevels(trees$species)
+levels(trees$species)
+
+
 # Add cluster ID to trees data set
 trees <- merge(trees, plots[, c("plot.id", "cluster")], sort=FALSE, by = "plot.id", all=FALSE)
 # Add cluster and plot ID and species to years data set
-years <- merge(years, trees[, c("tree.id", "plot.id", "cluster", "species")], sort=FALSE, by = "tree.id", all=FALSE)
+years <- merge(years, trees[, c("tree.id", "plot.id", "cluster", "species", "voronoi.area")], sort=FALSE, by = "tree.id", all=FALSE)
 
 # how many trees have BAI data for each year?
 with(years, table(bai.pres=!is.na(bai.comb), year)) # most years have values for most trees
@@ -43,5 +51,22 @@ p
 # No obvious difference between trees' basal area that's measured from core out (ba, bai) and basal area that's measured from outside in (ba.ext, bai.ext). Therefore, proceed with using the combined data for now (ba.comb, bai.comb). 
 
 
+#### Simplistic analysis: are there differences in growth rates by species? 
 
+years$ba.prev.comb.std <- scale(years$ba.prev.comb)
+years$ba.comb.std <- scale(years$ba.comb)
+m <- lm(ba.comb.std ~ ba.prev.comb.std*species, data=years)
+summary(m)
 
+# Are there differences in sensitivity to ppt by species? 
+m <- lmer(bai.comb ~ ba.prev.comb.std*species + ppt.z*species + (1|tree.id), data=years)
+summary(m) # not a ton, but 
+
+# Are there differences in sensitivity to ppt by cluster within PSME? 
+years_psme <- years[which(years$species=="PSME"),]
+m <- lmer(bai.comb ~ ba.prev.comb.std*cluster + ppt.z*cluster + (1|plot.id/tree.id), data=years_psme)
+summary(m) # Yosemite is more sensitive than the rest of the clusters 
+
+ggplot(years_psme[years_psme$plot.id=="HEN1",], aes(ppt, bai.comb)) + geom_point() + facet_wrap(~tree.id)
+
+ggplot(years_psme, aes(voronoi.area, bai.comb)) + geom_point() + facet_wrap(~plot.id)
